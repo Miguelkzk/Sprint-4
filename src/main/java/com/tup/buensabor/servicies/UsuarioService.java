105f534e56d09064fc5c01a2c7739a3486901e79
Lucio25 <luciomalgiogliopl3@gmail.com>
2023-10-25 10:07:53 -0300

    Commit HU login

--- src/main/java/com/tup/buensabor/servicies/UsuarioService.java
+++ src/main/java/com/tup/buensabor/servicies/UsuarioService.java
@@ -1,6 +1,10 @@
 package com.tup.buensabor.servicies;
 
+import com.tup.buensabor.entities.Cliente;
 import com.tup.buensabor.entities.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface UsuarioService extends BaseService<Usuario,Long> {
+    Page<Usuario> searchUsuario(String filtro1, String filtro2, Pageable pageable) throws Exception;
 }
